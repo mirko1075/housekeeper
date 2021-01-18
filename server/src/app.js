134d7560b49b7f9ca31031eef2d16748cd24060c
Mirko Siddi <mirko.siddi@gmail.com>
2021-01-18 15:34:07 +0100

    Add packages to package.json

---
+++ server/src/app.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const cors = require("cors");
+
+require("dotenv").config();
+
+const authRouter = require("./routes/auth.router");
+const apiRouter = require("./routes/api.router");
+const privateRouter = require("./routes/private.router");
+
+// MONGOOSE CONNECTION
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Connected to database`))
+  .catch((err) => console.error(err));
+
+// EXPRESS SERVER INSTANCE
+const app = express();
+
+// CORS MIDDLEWARE SETUP
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000", "http://localhost", "http://127.0.0.1"],
+  })
+);
+
+// SESSION MIDDLEWARE
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 30 * 24 * 60 * 60, // 30 days
+    }),
+    secret: process.env.SECRET_SESSION,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+// MIDDLEWARE
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// ROUTER MIDDLEWARE
+app.use("/auth", authRouter);
+app.use("/api", apiRouter);
+app.use("/private", privateRouter);
+// ROUTE FOR SERVING REACT APP (index.html)
+app.use((req, res, next) => {
+  // If no previous routes match the request, send back the React app.
+  res.sendFile(__dirname + "/public/index.html");
+});
+// ERROR HANDLING
+//  Catch 404 and respond with error message
+// Shows a 404 error with a message when no route is found for the request
+app.use((req, res, next) => {
+  res.status(404).json({ code: "not found" }); // .send( JSON.stringify(  { code: 'not found' }  ) )
+});
+
+// Catch `next(err)` calls
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error("ERROR", req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    const statusError = err.status || "500";
+    res.status(statusError).json(err);
+  }
+});
+
+module.exports = app;
