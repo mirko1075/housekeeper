bb63951cbd21a48f8af76479d80f6585cbf45203
Mirko Siddi <mirko.siddi@gmail.com>
2021-01-18 16:58:11 +0100

    Completed auth API

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,22 +1,23 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  
-    username: {type: String, required: true},
-    email: {type: String, unique: true, required: true},
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
     password: String,
     image: String,
-    admin: {type: Boolean, default: false},
-    household: {type: Schema.Types.ObjectId, ref:"Household"},
-   	currentScore: {type: Number, deafult: 0, required: true},
-   	shifts: [{type: Schema.Types.ObjectId, ref:"Shift"}],
-   	pendingTasks: [{type: Schema.Types.ObjectId, ref:"Task"}],
-    completedTasks: [{type: Schema.Types.ObjectId, ref:"Task"}],
-}, 
-{
-  timestamps: true
-});
+    admin: { type: Boolean, default: false },
+    household: { type: Schema.Types.ObjectId, ref: "Household" },
+    currentScore: { type: Number, default: 0 },
+    shifts: [{ type: Schema.Types.ObjectId, ref: "Shift" }],
+    pendingTasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+    completedTasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
