586d6b17b06be664c88b19be06d3db7a6b580fbf
Mirko Siddi <mirko.siddi@gmail.com>
2021-01-19 16:46:43 +0100

    Setting up cloudinary backend, not working yet

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -3,63 +3,49 @@ const router = express.Router();
 const createError = require("http-errors");
 const mongoose = require("mongoose");
 const User = require("../models/user.model");
+const uploader = require("../config/cloudinary-setup");
 
 const {
-    isLoggedIn,
-    isNotLoggedIn,
-    validationLogin,
-  } = require("../helpers/middlewares");
-
-// router.get("/user", isLoggedIn, (req, res, next) => {
-//     const userId = req.session.currentUser._id;
-  
-//     User.findById(userId)
-//       .then((user) => {
-//         if (!user) {
-//           return next(createError(404)); // Not Found
-//         }
-//         res.status(200).json(user);
-//       })
-//       .catch((err) => {
-//         next(createError(err));
-//       });
-//   });
-
+  isLoggedIn,
+  isNotLoggedIn,
+  validationLogin,
+} = require("../helpers/middlewares");
 
 router.put("/edit", isLoggedIn, (req, res, next) => {
-    const {username, image} = req.body;
-    const userId = req.session.currentUser._id;
-  
-    User.findByIdAndUpdate(userId, {username, image}, {new: true})
+  const { username, image } = req.body;
+  const userId = req.session.currentUser._id;
+
+  User.findByIdAndUpdate(userId, { username, image }, { new: true })
     .then((modifiedUser) => {
-        res
-        .status(200) 
-        .json(modifiedUser); 
+      res.status(200).json(modifiedUser);
     })
     .catch((err) => {
-        next(createError(err)); 
+      next(createError(err));
     });
-      
 });
 
-router.delete('/:id', isLoggedIn, (req, res)=>{
-    const { id } = req.params;
-    if ( !mongoose.Types.ObjectId.isValid(id)) {
-      res.status(400).json({ message: 'Specified id is not valid' });
-      return;
-    }
-    User.findByIdAndRemove(id)
-      .then(() => {
-          req.session.destroy();
-        res
-          .status(202)  
-          .send(`Document ${id} was removed successfully.`);
-      })
-      .catch( err => {
-        res.status(500).json(err);
-      })
+router.delete("/:id", isLoggedIn, (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+  User.findByIdAndRemove(id)
+    .then(() => {
+      req.session.destroy();
+      res.status(202).send(`Document ${id} was removed successfully.`);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
-
+router.post("/upload", uploader.single("image"), (req, res, next) => {
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  res.json({ secure_url: req.file.secure_url });
+});
 
 module.exports = router;
