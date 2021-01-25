9c785169504b09d3d1b2fa65033ae8c5749da9fb
Mirko Siddi <mirko.siddi@gmail.com>
2021-01-25 17:14:39 +0100

    Created socket.io

--- housekeeper/src/pages/Authentication.js
+++ housekeeper/src/pages/Authentication.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import authService from "./../lib/auth-service";
-import {withAuth} from './../context/auth-context';
+import { withAuth } from "./../context/auth-context";
 
 class Authentication extends Component {
-
   state = {
     username: "",
     email: "",
@@ -25,90 +23,88 @@ class Authentication extends Component {
     });
   };
 
-    submitLogin = (event) => {
-        event.preventDefault();
-        const {email, password} = this.state;
-        this.props.login(email,password);
-    }
+  submitLogin = (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    this.props.login(email, password);
+  };
 
   submitSignup = (event) => {
     event.preventDefault();
-        const {username, email, password} = this.state;
-        this.props.signup(username, email,password);
+    const { username, email, password } = this.state;
+    this.props.signup(username, email, password);
   };
 
   render() {
-
     return (
-        <div>
-        {!this.state.showSignup 
-        ? (<div>
-        <h2>Login form</h2>
-        <form onSubmit={(e) => this.submitLogin(e)}>
-          <label>Email: </label> <br />
-          <input
-            type="text"
-            required
-            name="email"
-            value={this.state.email}
-            onChange={(e) => this.handleInput(e)}
-          />{" "}
-          <br />
-          <label>Password: </label> <br />
-          <input
-            type="password"
-            required
-            name="password"
-            value={this.state.password}
-            onChange={(e) => this.handleInput(e)}
-          />{" "}
-          <br />
-          <button type="submit">Submit</button>
-        </form>
-        Don't have an account? <br />
-        <button onClick={this.toggleSignup}>Sign Up</button>
-      </div>) 
-      : (
       <div>
-        <h2>Signup form</h2>
-        <form onSubmit={(e) => this.submitSignup(e)}>
-          <label>Username: </label> <br />
-          <input
-            type="text"
-            required
-            name="username"
-            value={this.state.username}
-            onChange={(e) => this.handleInput(e)}
-          />
-          <br />
-          <label>Email: </label> <br />
-          <input
-            type="text"
-            required
-            name="email"
-            value={this.state.email}
-            onChange={(e) => this.handleInput(e)}
-          />
-          <br />
-          <label>Password: </label> <br />
-          <input
-            type="password"
-            required
-            name="password"
-            value={this.state.password}
-            onChange={(e) => this.handleInput(e)}
-          />
-          <br />
-          <button type="submit">Submit</button>
-        </form>
-        Already have an account? <br />
-        <button onClick={this.toggleSignup}>Log In</button>
-      </div>)} 
-    
-    </div>
-    )
-       
-}};
-
+        {!this.state.showSignup ? (
+          <div>
+            <h2>Login form</h2>
+            <form onSubmit={(e) => this.submitLogin(e)}>
+              <label>Email: </label> <br />
+              <input
+                type="text"
+                required
+                name="email"
+                value={this.state.email}
+                onChange={(e) => this.handleInput(e)}
+              />{" "}
+              <br />
+              <label>Password: </label> <br />
+              <input
+                type="password"
+                required
+                name="password"
+                value={this.state.password}
+                onChange={(e) => this.handleInput(e)}
+              />{" "}
+              <br />
+              <button type="submit">Submit</button>
+            </form>
+            Don't have an account? <br />
+            <button onClick={this.toggleSignup}>Sign Up</button>
+          </div>
+        ) : (
+          <div>
+            <h2>Signup form</h2>
+            <form onSubmit={(e) => this.submitSignup(e)}>
+              <label>Username: </label> <br />
+              <input
+                type="text"
+                required
+                name="username"
+                value={this.state.username}
+                onChange={(e) => this.handleInput(e)}
+              />
+              <br />
+              <label>Email: </label> <br />
+              <input
+                type="text"
+                required
+                name="email"
+                value={this.state.email}
+                onChange={(e) => this.handleInput(e)}
+              />
+              <br />
+              <label>Password: </label> <br />
+              <input
+                type="password"
+                required
+                name="password"
+                value={this.state.password}
+                onChange={(e) => this.handleInput(e)}
+              />
+              <br />
+              <button type="submit">Submit</button>
+            </form>
+            Already have an account? <br />
+            <button onClick={this.toggleSignup}>Log In</button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default withAuth(Authentication);
